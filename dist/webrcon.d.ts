6f1ecacb03e93467a4e8397d0926e8c3c3fa0351
Timo Salola <timo@salola.fi>
2018-03-04 17:08:59 +0200

    Support sending commands

--- dist/webrcon.d.ts
+++ dist/webrcon.d.ts
@@ -2,7 +2,12 @@ export declare class WebRcon {
     private _connection;
     private _address;
     private _password;
+    private _seq;
+    private _resolves;
     static connect: (address: string, password: string) => Promise<WebRcon>;
     constructor(address: string, password: string);
-    connect: () => Promise<WebRcon>;
+    connect(): Promise<WebRcon>;
+    command: (command: string) => Promise<string>;
+    private _handleMessage;
+    private _parsePacket;
 }
--- lib/webrcon.ts
+++ lib/webrcon.ts
@@ -1,9 +1,12 @@
-import ws from 'ws';
+import * as ws from 'ws';
+import { ResolveArray, WebRconSendPacket, WebRconReceivePacket } from './interfaces';
 
 export class WebRcon {
     private _connection: ws | undefined;
     private _address: string;
     private _password: string;
+    private _seq: number;
+    private _resolves: ResolveArray<string>;
 
     static connect = async (address: string, password: string): Promise<WebRcon> => {
         const webRcon = new WebRcon(address, password);
@@ -14,14 +17,44 @@ export class WebRcon {
         this._address = address;
         this._password = password;
         this._connection = undefined;
+        this._seq = 1;
+        this._resolves = [];
     }
 
-    connect = async (): Promise<WebRcon> => {
+    connect(): Promise<WebRcon> {
         return new Promise<WebRcon>((resolve, reject) => {
             this._connection = new ws(`${this._address}/${this._password}`);
             this._connection.on('open', () => {
                 resolve(this);
             });
+            this._connection.on('message', this._handleMessage);
         });
     }
+
+    command = async (command: string): Promise<string> => {
+        return new Promise<string>((resolve, reject) => {
+            const identifier = this._seq++;
+            this._resolves[identifier] = resolve;
+
+            const packet: WebRconSendPacket = {
+                Identifier: identifier,
+                Message: command,
+                Name: 'WebRcon'
+            };
+
+            this._connection!.send(JSON.stringify(packet));
+        });
+    };
+
+    private _handleMessage = (data: ws.Data) => {
+        const packet = this._parsePacket(data as string);
+        
+        if (!this._resolves[packet.Identifier]) {
+            // Some other message...
+        }
+
+        this._resolves[packet.Identifier](packet.Message);
+    }
+
+    private _parsePacket = (data: string): WebRconReceivePacket => JSON.parse(data);
 }
